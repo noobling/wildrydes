46b9c0d8632201fbca1a44605d7e1491d0b2a138
0x0 <ghstoreinvesting@gmail.com>
2019-01-01 11:14:35 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_PVEgDFvsS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '152ktgucmnfagrmbms02s4k1vf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
